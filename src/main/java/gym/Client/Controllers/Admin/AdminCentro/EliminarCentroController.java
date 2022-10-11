b09a1b0e31652599b9b01c842fa27f53fe3aab53
GermanDellaNave <gdellanave@correo.um.edu.uy>
2022-10-11 15:01:40 -0300

    Cancelar Eliminar centro actualizado

--- src/main/java/gym/Client/Controllers/Admin/AdminCentro/EliminarCentroController.java
+++ src/main/java/gym/Client/Controllers/Admin/AdminCentro/EliminarCentroController.java
@@ -1,6 +1,9 @@
 package gym.Client.Controllers.Admin.AdminCentro;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 import javafx.scene.control.Button;
@@ -30,7 +33,13 @@ public class EliminarCentroController {
     }
 
     @FXML
-    protected void onCancelarButtonClick() {}
+    protected void onCancelarButtonClick(ActionEvent event) {
+        emailText.clear();
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+
+    }
 
 
 }
