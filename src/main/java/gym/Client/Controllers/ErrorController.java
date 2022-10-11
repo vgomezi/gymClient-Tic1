90f569118ce4e923a237688ab9ecec826ef6a958
vgomezi <vgomez2@correo.um.edu.uy>
2022-10-11 18:08:18 -0300

    formulario y controlers de error

---
+++ src/main/java/gym/Client/Controllers/ErrorController.java
@@ -0,0 +1,26 @@
+package gym.Client.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ErrorController {
+
+
+    @FXML
+    private Label errorText;
+
+    @FXML
+    private Label whichErrorText;
+
+}
