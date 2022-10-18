76bb19ef0ce77226a9c8fbabc8f72795d509af19
GermanDellaNave <gdellanave@correo.um.edu.uy>
2022-10-18 20:31:08 -0300

    Get funcionando LoginController

--- src/main/java/gym/Client/Controllers/LoginController.java
+++ src/main/java/gym/Client/Controllers/LoginController.java
@@ -50,114 +50,145 @@ public class LoginController {
         String contrasena = contrasenaText.getText();
 
         if (!correoElectronico.isEmpty() && !contrasena.isEmpty()) {
+            String role = "";
             try {
-                String json = "";
-                try {
-                    ObjectMapper mapper = new ObjectMapper();
-                    ObjectNode rest = mapper.createObjectNode();
-
-
-                } catch (Exception e) {
-                }
                 HttpResponse<String> apiResponse = null;
 
                 apiResponse = Unirest.get("http://localhost:8987/api/login/role/" + correoElectronico).asObject(String.class);
-                System.out.println(apiResponse.getBody().toString());
-
+                role = apiResponse.toString();
+                System.out.println(apiResponse);
             }catch (Exception e) {
+            }
+            if (role.equals("Admin")) {
+                
+                Node source = (Node) event.getSource();
+                Stage stage1 = (Stage) source.getScene().getWindow();
+                stage1.close();
+                
+                try {
+                    
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesAdministrador/MainAdmin.fxml"));
+
+                    Stage stage = new Stage();
+
+                    stage.initModality(Modality.APPLICATION_MODAL);
+
+                    stage.setTitle("Login Admin");
+                    stage.getIcons().add(new Image("GymIcon.png"));
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                    System.out.println("Error");
+                }
+            } else if (role.equals("Empresa")) {
+                Node source = (Node) event.getSource();
+                Stage stage1 = (Stage) source.getScene().getWindow();
+                stage1.close();
+
+                try {
+
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesEmpresaPrueba/MainEmpresa.fxml"));
+
+                    Stage stage = new Stage();
+
+                    stage.initModality(Modality.APPLICATION_MODAL);
+
+                    stage.setTitle("Login Empresa");
+                    stage.getIcons().add(new Image("GymIcon.png"));
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                    System.out.println("Error");
+                }
+                
+            } else if (role.equals("Centro Deportivo")) {
+                Node source = (Node) event.getSource();
+                Stage stage1 = (Stage) source.getScene().getWindow();
+                stage1.close();
+
+                try {
+
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesCentroPrueba/MainCentro.fxml"));
+
+                    Stage stage = new Stage();
+
+                    stage.initModality(Modality.APPLICATION_MODAL);
+
+                    stage.setTitle("Login Centro Deportivo");
+                    stage.getIcons().add(new Image("GymIcon.png"));
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                    System.out.println("Error");
+                }
+            } else if (role.equals("Usuario")) {
+                Node source = (Node) event.getSource();
+                Stage stage1 = (Stage) source.getScene().getWindow();
+                stage1.close();
+
+                try {
+
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesUsuarioPrueba/MainUsuario.fxml"));
+
+                    Stage stage = new Stage();
+
+                    stage.initModality(Modality.APPLICATION_MODAL);
+
+                    stage.setTitle("Login Centro Deportivo");
+                    stage.getIcons().add(new Image("GymIcon.png"));
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                    System.out.println("Error");
+                }
+            } else {
+                try {
+
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/gym/Client/Error.fxml"));
+
+                    Stage stage = new Stage();
+
+                    stage.initModality(Modality.APPLICATION_MODAL);
+
+                    stage.setTitle("Error");
+                    stage.getIcons().add(new Image("GymIcon.png"));
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
+                    System.out.println("Error");
+                }
 
             }
 
         } else {
             emailText.clear();
             contrasenaText.clear();
-        }
 
-        Node source = (Node) event.getSource();
-        Stage stage1 = (Stage) source.getScene().getWindow();
-        stage1.close();
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            //Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesAdministrador/MainAdmin.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesEmpresaPrueba/MainEmpresa.fxml"));
-
-            Stage stage = new Stage();
-
-            stage.initModality(Modality.APPLICATION_MODAL);
-
-            stage.setTitle("Login Empresa");
-            stage.getIcons().add(new Image("GymIcon.png"));
-            stage.setScene(new Scene(root1));
-            stage.show();
-
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-            System.out.println("Error");
-            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /*
-    * String nR = nombre.getText();
-        String tR = telefono.getText();
-        String dR = direccion.getText();
-
-        if (!nR.isEmpty() && !dR.isEmpty() && !tR.isEmpty()) {
             try {
-                Integer tR2 = Integer.parseInt(telefono.getText());
-                try {
-                    String json = "";
 
-                    try {
-                        ObjectMapper mapper = new ObjectMapper();
-                        ObjectNode rest = mapper.createObjectNode();
-                        rest.put("nombre", nR);
-                        rest.put("telefono", tR);
-                        rest.put("direccion", dR);
-                        json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rest);
-                    } catch (Exception ignored) {
-                    }
-                    HttpResponse<JsonNode> apiResponse = null;
-                    try {
-                        apiResponse = Unirest.post("http://localhost:8987/api/restaurant").header("Content-Type", "application/json").body(json).asJson();
-
-                    } catch (UnirestException el) {
-                        throw new RuntimeException(el);
-                    }
-
-                    welcomeText.setText("Se creó el restaurante!");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    welcomeText.setText("Restaurante ya registrado");
-                }
-                nombre.clear();
-                telefono.clear();
-                direccion.clear();
-            } catch (Exception E) {
-                welcomeText.setText("Teléfono no válido");
-            }
-        } else {
-            nombre.clear();
-            telefono.clear();
-            direccion.clear();
-            welcomeText.setText("Error en la entrada de datos");
-        }
-*/
-
-    @FXML
-    protected void onCancelarButtonClick(ActionEvent event) {
-        if (contrasenaText.getText().isEmpty()) {
-            Node source = (Node) event.getSource();
-            Stage stage1 = (Stage) source.getScene().getWindow();
-            stage1.close();
-            try {
                 FXMLLoader fxmlLoader = new FXMLLoader();
-                Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/OpcionesAdministrador/AdminCentro/MainAdminCentro.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/formularios/gym/Client/Error.fxml"));
+
                 Stage stage = new Stage();
 
                 stage.initModality(Modality.APPLICATION_MODAL);
 
-                stage.setTitle("Login Centro");
+                stage.setTitle("Error");
                 stage.getIcons().add(new Image("GymIcon.png"));
                 stage.setScene(new Scene(root1));
                 stage.show();
@@ -165,19 +196,9 @@ public class LoginController {
             } catch (Exception ex) {
                 System.out.println(ex.toString());
                 System.out.println("Error");
-                //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } else {
-            emailText.clear();
-            contrasenaText.clear();
-            Node source = (Node) event.getSource();
-            Stage stage = (Stage) source.getScene().getWindow();
-            stage.close();
-            System.exit(0);
+
         }
     }
-
-
-
 
 }
