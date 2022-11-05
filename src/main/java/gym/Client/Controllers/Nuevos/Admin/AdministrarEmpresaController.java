bce8f252507f984ce458bc2830cfcf66d5676bae
vgomezi <106179011+vgomezi@users.noreply.github.com>
2022-11-05 00:09:39 -0300

    mouse logout

--- src/main/java/gym/Client/Controllers/Nuevos/Admin/AdministrarEmpresaController.java
+++ src/main/java/gym/Client/Controllers/Nuevos/Admin/AdministrarEmpresaController.java
@@ -1,11 +1,18 @@
 package gym.Client.Controllers.Nuevos.Admin;
 
+import gym.Client.Controllers.LoginController;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -13,6 +20,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class AdministrarEmpresaController {
 
@@ -72,23 +81,59 @@ public class AdministrarEmpresaController {
 
     }
 
+
     @FXML
-    void onMisActividadesLabelClick(MouseEvent event) {
+    void onMouseClickedLogOut(MouseEvent mouseEvent) {
+        Node source = (Node) mouseEvent.getSource();
+        Stage stage1 = (Stage) source.getScene().getWindow();
+        stage1.close();
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/gym/Client/Login.fxml"));
+
+            Stage stage = new Stage();
+
+            stage.setTitle("Login");
+            stage.setIconified(false);
+            stage.setResizable(false);
+            stage.getIcons().add(new Image("FitnessIcon.png"));
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    System.exit(0);
+                }
+            });
+
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            System.out.println("Error");
+        }
+
+    }
+
+
+    @FXML
+    void onAdministrarEmpresaLabelClick(MouseEvent event) {
 
     }
 
     @FXML
-    void onMouseClickedLogOut(MouseEvent event) {
+    void onTodasLasEmpresasLabelClick(MouseEvent event) {
 
     }
 
     @FXML
-    void onReservarActividadClick(ActionEvent event) {
+    void onActualizarEmpresaButtonClick(MouseEvent event) {
 
     }
 
     @FXML
-    void onTodasLasActividadesLabelClick(MouseEvent event) {
+    void onEliminarEmpresaButtonClick(MouseEvent event) {
 
     }
 
--- src/main/java/gym/Client/Controllers/Nuevos/AdministrarCentroDeudaController.java
+++ src/main/java/gym/Client/Controllers/Nuevos/AdministrarCentroDeudaController.java
@@ -1,11 +1,20 @@
 package gym.Client.Controllers.Nuevos;
 
+import gym.Client.Controllers.LoginController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class AdministrarCentroDeudaController {
 
@@ -37,6 +46,35 @@ public class AdministrarCentroDeudaController {
 
     @FXML
     void onMouseClickedLogOut(MouseEvent mouseEvent) {
+        Node source = (Node) mouseEvent.getSource();
+        Stage stage1 = (Stage) source.getScene().getWindow();
+        stage1.close();
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Parent root1 = (Parent) fxmlLoader.load(LoginController.class.getResourceAsStream("/gym/Client/Login.fxml"));
+
+            Stage stage = new Stage();
+
+            stage.setTitle("Login");
+            stage.setIconified(false);
+            stage.setResizable(false);
+            stage.getIcons().add(new Image("FitnessIcon.png"));
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    System.exit(0);
+                }
+            });
+
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            System.out.println("Error");
+        }
 
     }
 
